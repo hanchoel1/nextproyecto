ca8ea80d544c9366f10b7546290a1f39acff0e6a
hanchoel1 <hanchoel16@gmail.com>
2024-03-05 22:45:22 -0400

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import {Inter} from 'next/font/google';
+import {Lusitana} from 'next/font/google';
+ 
+const inter = Inter({ subsets: ['latin'] });
+
+const lusitana = Lusitana({weight :"700", subsets: ['latin']});
+
+
+export {inter,lusitana};
